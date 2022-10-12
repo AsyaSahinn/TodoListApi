b32b529f1a71e2710e3cf443abee580bdc58e8fc
asyasahin <asyasahin@example.com>
2022-10-12 17:52:56 +0300

    Models/TodoDBContext.cs modified

--- Models/TodoDBContext.cs
+++ Models/TodoDBContext.cs
@@ -1,6 +1,26 @@
-﻿namespace TodoListApp.Models
+﻿using Microsoft.Extensions.Configuration;
+//using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Models
 {
-    public class TodoDBContext
+    class AppConfig
     {
+        public static string getAppSettingKey(string key)
+        {
+            var builder = new ConfigurationBuilder()
+                   .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json");
+            IConfigurationRoot Configuration = builder.Build();
+            var connectionString = Configuration[key];
+            return connectionString;
+        }
     }
+
 }

b91d03f1f3f8557b47444c0e9cf4aef06a7762a1
asyasahin <asyasahin@example.com>
2022-10-12 17:21:53 +0300

    Models/TodoItem.cs modified

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -2,5 +2,11 @@
 {
     public class TodoItem
     {
+        public int Id { get; set; } 
+
+        public string? Name { get; set; }    
+
+        public bool IsCompleted { get; set; }
+
     }
 }
